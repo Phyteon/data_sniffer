21b154ac9adbe4de2dde25565b0bd160fc18ca9d
Filip Pawelec <pawelecfilip6@gmail.com>
2022-06-26 16:56:30 +0200

    Fixed gitignore, added files.

---
+++ data_sniffer.srcs/sources_1/new/data_converter.sv
@@ -0,0 +1,12 @@
+
+
+module data_converter
+    (
+        input
+            logic clk,
+            logic nreset,
+            wire [7:0] data_chunk,
+        output wire [7:0] data_processed
+    );
+    
+endmodule
---
+++ data_sniffer.srcs/sources_1/new/rem_div_element.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.06.2022 16:32:13
+// Design Name: 
+// Module Name: rem_div_element
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rem_div_element(
+
+    );
+endmodule
